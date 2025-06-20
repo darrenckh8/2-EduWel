4d2eb7abec6ada4f3d183419b61448736904db5e
Darren <darrenckh8@gmail.com>
2025-06-20 19:28:52 +0800

    Updated CH3

--- EDU PICO/CH3/3.neopixelRGBLED.py
+++ EDU PICO/CH3/3.neopixelRGBLED.py
@@ -1,14 +1,44 @@
-import board # Helps us refer to the correct pins on the board.                             
-import time # Used to pause the program for a short time.                                    
-import neopixel # Allows us to interact with the neopixel RGB LED.                           
+import board
+import time
+import digitalio
+from analogio import AnalogIn
+import neopixel
 
-num_pixels = 1 # Set the number of pixels to 1.
-pixel_pin = board.GP14 # Set the pin connected to the neopixel RGB LED to pin 14.
+# Setup for potentiometer (CH2)
+pot = AnalogIn(board.GP28)
 
-# Create a variable called pixels that represents the neopixel RGB LED connected to pin 14.
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2) 
+# Setup for button (CH1)
+button = digitalio.DigitalInOut(board.GP0)  # Use GP0 for button
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.UP
 
-while True: # Create an infinite loop that will run forever.
-    pixels.fill((255, 255, 255)) # Turn on the neopixel RGB LED to white.
-    time.sleep(1) # Pause the program for 1 second.
-    
+# Setup for NeoPixel RGB LED (CH3)
+pixel_pin = board.GP14
+num_pixels = 1
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2)
+
+# State for color selection: 0=R, 1=G, 2=B
+color_select = 0
+color_names = ['Red', 'Green', 'Blue']
+color = [0, 0, 0]
+
+last_button = True
+
+while True:
+    # Read button (active low)
+    current_button = button.value
+    if not current_button and last_button:
+        color_select = (color_select + 1) % 3  # Cycle R->G->B->R
+        print(f"Selected: {color_names[color_select]}")
+        time.sleep(0.2)  # Debounce
+    last_button = current_button
+
+    # Read potentiometer and scale to 0-255
+    pot_value = int((pot.value * 255) / 65535)
+    color[color_select] = pot_value
+
+    # Set NeoPixel color
+    pixels.fill(tuple(color))
+
+    print(f"R: {color[0]}, G: {color[1]}, B: {color[2]} (Selected: {color_names[color_select]})")
+    time.sleep(0.05)
